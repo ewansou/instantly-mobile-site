b0d39e698a541215ddc9946ea61a26647f665c6f
truonghx <truonghx87@bmail.vn>
2014-05-06 18:37:35 +0700

    fix crop function bug

--- public/js/crop-03.js
+++ public/js/crop-03.js
@@ -1,18 +1,16 @@
-$(function () {
-      $( '.cropimage' ).each( function () {
-        var image = $(this),
-            cropwidth = 320,
-            cropheight = 400;
+$( document ).ready(function() {
+    var image = $('.cropimage'),
+        cropwidth = 320,
+        cropheight = 320;
 
-          var x = image.cropbox( {width: cropwidth, height: cropheight, showControls: 'always', zoom : 20} )
-            .on('cropbox', function( event, results, img ) {
-              $('#x').val(results.cropX);
-              $('#y').val(results.cropY);
-              $('#w').val(results.cropW);
-              $('#h').val(results.cropH);
-            });
+    var x = image.cropbox( {width: cropwidth, height: cropheight, showControls: 'always'} )
+      .on('cropbox', function( event, results, img ) {
+        $('#x').val(results.cropX);
+        $('#y').val(results.cropY);
+        $('#w').val(results.cropW);
+        $('#h').val(results.cropH);
+      });
 
-      } );
 
       //$(".cropZoomIn").trigger('click');
 
@@ -23,5 +21,4 @@ $(function () {
       function showLoader () {
         $(".backdrop").removeClass('hide').addClass('in');
       }
-
 });
